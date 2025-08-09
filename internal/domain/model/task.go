aa96806baf488e8d5cfaccc6c545437a068dfdd8
OTakumi <takumi.o.0410@gmail.com>
2025-08-09 19:10:42 +0900

    feat(model): Task生成関数とテストを追加
    Task構造体の新規生成用NewTask関数を追加しました。UUID生成と初期値
    設定を行います。また、NewTaskおよびValidateメソッドの単体テストを
    新規作成し、正常系・異常系の動作を検証しています。

--- internal/domain/model/task.go
+++ internal/domain/model/task.go
@@ -16,6 +16,22 @@ type Task struct {
 	UpdatedAt  time.Time
 }
 
+func NewTask(title string) *Task {
+	// UUIDを生成する
+	uuid, err := uuid.NewUUID()
+	if err != nil {
+		panic(err)
+	}
+
+	return &Task{
+		ID:         uuid,
+		Title:      title,
+		IsComplete: false,
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+	}
+}
+
 func (t *Task) Validate() error {
 	if t.Title == "" {
 		return errors.New("Title is required")
---
+++ internal/domain/model/task_test.go
@@ -0,0 +1,78 @@
+package model_test
+
+import (
+	"OTakumi/todogo/internal/domain/model"
+	"testing"
+	"time"
+)
+
+func TestNewTask(t *testing.T) {
+	t.Run("NewTask関数がTaskを問題なく生成すること", func(t *testing.T) {
+		// Arrange
+		title := "Testing Go"
+
+		// Act
+		task := model.NewTask(title)
+
+		// Assert
+		// オブジェクトがnilでないこと
+		if task == nil {
+			t.Fatal("NewTask() returned nil")
+		}
+
+		// Titleが引数の通りに設定されていること
+		if task.Title != title {
+			t.Errorf("expected Title to be '%s', but got '%s'", title, task.Title)
+		}
+
+		// 作成直後なので、IsCompleteがfalseになっていること
+		if task.IsComplete != false {
+			t.Errorf("expected Completed to be false, but got %v", task.IsComplete)
+		}
+
+		// CreatedAtに作成日時が設定されていること
+		if task.CreatedAt.IsZero() || time.Since(task.CreatedAt) > time.Second {
+			t.Errorf("expected CreatedAt to be set to the current time, but got %v", task.CreatedAt)
+		}
+
+		// UpdatedAtに作成日時が設定されていること
+		if task.UpdatedAt.IsZero() || time.Since(task.UpdatedAt) > time.Second {
+			t.Errorf("expected UpdatedAt to be set to the current time, but got %v", task.UpdatedAt)
+		}
+	})
+}
+
+func TestTask_Validate(t *testing.T) {
+	t.Run("タスクのタイトルが空の場合、エラーが返されること", func(t *testing.T) {
+		// Arrange
+		task := model.Task{
+			Title: "",
+		}
+
+		// Act
+		err := task.Validate()
+
+		// Assert
+		// Validateがエラーを返す
+		// nilの場合、テストが失敗する
+		if err == nil {
+			t.Error("Expected an error, but go nil")
+		}
+	})
+
+	t.Run("タスクのタイトルが空でない場合、nilが返されること", func(t *testing.T) {
+		// Arrange
+		task := model.Task{
+			Title: "Test Task",
+		}
+
+		// Act
+		err := task.Validate()
+		// Assert
+		// Validateがnilを返す
+		if err != nil {
+			// エラーの中身を確認する
+			t.Errorf("did not expect an error, but got: %v", err)
+		}
+	})
+}
