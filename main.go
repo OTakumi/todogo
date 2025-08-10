92ecc38b805892b31f6dd98efca0be246013d9d5
OTakumi <takumi.o.0410@gmail.com>
2025-08-10 19:10:26 +0900

    feat: DB接続情報を環境変数から取得するよう変更
    .envファイルおよびOS環境変数からDB接続情報を取得するように修正。
    .envファイルが存在しない場合はログ出力のみ行い、OS環境変数を優先。
    必須の環境変数が未設定の場合はエラーで終了するようにした。

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
 )
@@ -32,8 +35,29 @@ func setupDB(dbDriver string, dsn string) (*sql.DB, error) {
 }
 
 func main() {
+	// dotenvファイルから環境変数を読み込む
+	// 実行環境（本番環境など）に.envファイルがない場合でも、
+	// OSの環境変数が設定されていればそちらを優先して利用できるため、
+	// err != nil の場合はエラーとせず、ログ出力に留める。
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found")
+	}
+
+	// DBパラメータを環境変数から読み込む
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 	dbDriver := "postgres"
-	dsn := "host=localhost port=5433 user=postgres password=p@ssw0rd dbname=todogo sslmode=disable"
+
+	// 必須の環境変数が設定されているか確認
+	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" {
+		log.Fatal("Database environment variables are not set correctly")
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
 
 	db, err := setupDB(dbDriver, dsn)
 	if err != nil {
