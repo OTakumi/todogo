654dcde6c040c9c28354ccea9960031ea441af73
OTakumi <takumi.o.0410@gmail.com>
2025-08-10 19:51:43 +0900

    feat: PostgreSQLハンドラの追加
    新たにPostgreSQLHandler構造体と初期化関数を追加しました。
    データベース接続の確立、接続確認、コネクション管理を行います。
    今後のDB操作の基盤となる実装です。

---
+++ internal/infrastructure/postgres_handler.go
@@ -0,0 +1,36 @@
+package infrastructure
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type PostgreSQLHandler struct {
+	DB *sql.DB
+}
+
+func NewPostgreSQLHandler(dsn string) (*PostgreSQLHandler, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open posgresql: %w", err)
+	}
+
+	// 接続が実際に確立されるまで待機
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	// 接続確認
+	if err := db.Ping(); err != nil {
+		db.Close()
+
+		return nil, fmt.Errorf("Failed to ping postgres: %w", err)
+	}
+
+	log.Println("Successfully connected to the database.")
+	return &PostgreSQLHandler{DB: db}, err
+}
