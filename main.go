70d6793b9313b0f653a4cb69ffbb3af7367471cc
OTakumi <takumi.o.0410@gmail.com>
2025-08-10 17:02:02 +0900

    feat: PostgreSQLデータベース接続処理を追加
    main.goにPostgreSQLへの接続処理を追加しました。setupDB関数で
    接続設定・確認を行い、main関数で利用しています。今後のDB操作の
    基盤となります。

--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"OTakumi/todogo/cmd"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
+func setupDB(dbDriver string, dsn string) (*sql.DB, error) {
+	db, err := sql.Open(dbDriver, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// 接続が実際に確立されるまで待機
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	// 接続確認
+	if err := db.Ping(); err != nil {
+		db.Close()
+
+		return nil, fmt.Errorf("Failed to ping postgres: %w", err)
+	}
+
+	return db, err
+}
+
 func main() {
+	dbDriver := "postgres"
+	dsn := "host=localhost port=5433 user=postgres password=p@ssw0rd dbname=todogo sslmode=disable"
+
+	db, err := setupDB(dbDriver, dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
 	cmd.Execute()
 }
